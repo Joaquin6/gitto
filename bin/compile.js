7fbbdb4da458a4dbef63d31cf1469ab29f7ce789
Joaquin Briceno <joaquin.briceno@insitu.com>
2018-05-30 03:31:36 -0700

    added configs to support new JS syntax

---
+++ bin/compile.js
@@ -0,0 +1,43 @@
+const Promise = require('bluebird');
+const { exec } = require('pkg');
+
+const cliCommands = [
+  'inexa_cloud_upload',
+  'inexa_cloud_download',
+  'inexa_cloud_create_access_token_file',
+];
+
+function generateArgs(command, platform) {
+  if (platform) {
+    return [
+      `bin/${command}`,
+      '--targets',
+      `node6-${platform}-x64`,
+      '-o',
+      `out/${platform}/${command}`,
+    ];
+  }
+  return [
+    `bin/${command}`,
+    '--targets',
+    'node6',
+    '-o',
+    `out/${command}`,
+  ];
+}
+
+(async () => {
+  try {
+    if (process.argv[2] === '--all') {
+      await Promise.mapSeries(cliCommands, ((command) => exec(generateArgs(command, 'linux'))));
+      await Promise.mapSeries(cliCommands, ((command) => exec(generateArgs(command, 'win'))));
+    } else {
+      await Promise.mapSeries(cliCommands, ((command) => exec(generateArgs(command))));
+    }
+    console.log('Compile Complete');
+    process.exit();
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
